ea4ff06430030e43025abc409a810adc235f1420
Desiii-R <44482185+Desiii-R@users.noreply.github.com>
2018-10-26 09:59:40 +1100

    Arduino Code  : for prosthetic fingers
    This is the arduino code that use to run the project: Partial Pressure Sensitive Touch Prosthetic
    
    
    To run this project i use : 
    
    x1 Force sensitive resisitor
    x2  Micro Servo Motors (SG90) 
    x1 Mini Bread Board
    x1 Arduino Uno
    x1 4 AA Battery Pack 
    x1 25 volt  Capacitor

---
+++ Force_Sensitive_Resistor_Two_Servos/Force_Sensitive_Resistor_Two_Servos.ino
@@ -0,0 +1,32 @@
+ #include <Servo.h>
+ Servo servo1;// create a servo object to control servo1
+ Servo servo2; // create a servo object to control servo2
+
+ 
+ int PressureSensor = A0; //the number for the pressure sensor 
+ int val = 0; // the value in which each servo motor will read   
+ 
+void setup(void) {
+ Serial.begin(9600);   
+ servo1.attach(9);            //servo motor 1 will be attached at digital pin 9
+ servo1.write(0);             //initial reading for servo motor 1
+ servo2.attach(10);           //servo motor 2 will be attched to digital pin 10
+ servo2.write(0);             // initial reading reading for servo motor 2 
+
+}
+// the loop routine runs over and over again forever:
+ 
+//read the state of the pressure sensor value
+void loop(void) {
+  PressureSensor= analogRead(A0); // Pressure sensor will attached to analog 0
+  Serial.print("Sensor value = "); // the serial monitor will read sensor value 
+  Serial.println(PressureSensor); // the serial monitor will print the value the pressure sensor spits out
+ 
+
+int value = map(PressureSensor, 0, 1023, 0, 255); // the value that will calibrate the pressure sensors to move the servo motors 
+
+  servo1.write(value); // the value that will move the servo motor 1
+  servo2.write(value);.// the value that will move the servo motor 2
+ 
+  delay(100); // delay the loop for a few seconds 
+}
